af73a477f1c7f59c1b5873574b59880ef5f81e19
Brent Newbury <brent@brentnewbury.com>
2014-10-31 17:29:29 +0000

    Added unit test for name validation.

---
+++ Tests/Smaprat.Core.Tests/NameValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Smaprat.Core.Tests
+{
+    [TestClass]
+    public class NameValidatorTests
+    {
+        private static readonly string[] disallowedNames = new string[]
+        {
+            "me",
+            "admin",
+            "administrator",
+            "server",
+            "host"
+        };
+
+        [TestMethod]
+        public void EnsureBadNamesAreDisallowed()
+        {
+            NameValidator validator = new NameValidator();
+
+            foreach (string disallowedName in disallowedNames)
+                Assert.IsFalse(validator.IsValid(disallowedName));
+        }
+
+        [TestMethod]
+        public void EnsureBlankNamesAreDisallowed()
+        {
+            NameValidator validator = new NameValidator();
+
+            Assert.IsFalse(validator.IsValid(""));
+        }
+    }
+}
