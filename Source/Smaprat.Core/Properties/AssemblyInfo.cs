e526b45d1c08687d4f34e761fc76894b0955bf99
Brent Newbury <brent@brentnewbury.com>
2014-10-31 17:20:31 +0000

    Fixed issue where an AssemblyCultureAttribute is set to "en" making the compiled assemblies look like satelite assemblies.

--- Source/Smaprat.Core/Properties/AssemblyInfo.cs
+++ Source/Smaprat.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Smaprat.Core")]
-[assembly: AssemblyCulture("en")]
+[assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
